30773ab65b275e64651a8d7669d22f8d2084a276
Kavya Raj S B <kavyarajkavz2000@gmail.com>
2022-11-23 09:14:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,21 +27,21 @@ app.use(function (req, res, next) {
 });
 
 
-function verifyToken(req, res, next) {//token
-        if (!req.headers.authorization) {
-            return res.status(401).send('Unauthorized request')
-        }
-        let token = req.headers.authorization.split(' ')[1]
-        if (token === 'null') {
-            return res.status(401).send('Unauthorized request')
-        }
-        let payload = jwt.verify(token, 'secretKey')
-        if (!payload) {
-            return res.status(401).send('Unauthorized request')
-        }
-        req.userId = payload.subject
-        next()
-    }
+// function verifyToken(req, res, next) {//token
+//         if (!req.headers.authorization) {
+//             return res.status(401).send('Unauthorized request')
+//         }
+//         let token = req.headers.authorization.split(' ')[1]
+//         if (token === 'null') {
+//             return res.status(401).send('Unauthorized request')
+//         }
+//         let payload = jwt.verify(token, 'secretKey')
+//         if (!payload) {
+//             return res.status(401).send('Unauthorized request')
+//         }
+//         req.userId = payload.subject
+//         next()
+//     }
 
 
 const login = require('./routes/login'); //login page
